f3883346c713ec80c46aa11a2a4afcc986fefb0a
ivan00z96 <ivan_00z96@hotmail.com>
2015-03-04 20:08:07 -0600

    primera version del juego Space Invader

--- src/Sprite.java
+++ src/Sprite.java
@@ -1,5 +1,8 @@
 
 import java.awt.Image;
+import java.awt.Rectangle;
+import java.awt.image.ImageObserver;
+import javax.swing.ImageIcon;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -14,15 +17,38 @@ import java.awt.Image;
 public class Sprite {
 
         private boolean visible;
-        private Image image;
-        protected int x;
-        protected int y;
+        protected Image imaImagen;
+        protected int iX;
+        protected int iY;
         protected boolean dying;
         protected int dx;
+        public int iAncho; //ancho del objeto
+        public int iAlto; //largo del objeto
 
         public Sprite() {
             visible = true;
         }
+        
+//        /**
+//     * Sprite
+//     * 
+//     * Metodo constructor usado para crear el objeto animal
+//     * creando el icono a partir de una imagen
+//     * 
+//     * @param iX es la <code>posicion en x</code> del objeto.
+//     * @param iY es la <code>posicion en y</code> del objeto.
+//     * @param iAncho es el <code>ancho</code> del objeto.
+//     * @param iAlto es el <code>Largo</code> del objeto.
+//     * @param imaImagen es la <code>imagen</code> del objeto.
+//     * 
+//     */
+//    public Sprite(int iX, int iY , int iAncho, int iAlto,Image imaImagen) {
+//        this.iX = iX;
+//        this.iY = iY;
+//        this.iAncho = iAncho;
+//        this.iAlto = iAlto;
+//        this.imaImagen = imaImagen;
+//    }
 
         public void die() {
             visible = false;
@@ -36,27 +62,27 @@ public class Sprite {
             this.visible = visible;
         }
 
-        public void setImage(Image image) {
-            this.image = image;
+        public void setImage(Image imaImagen) {
+            this.imaImagen = imaImagen;
         }
 
         public Image getImage() {
-            return image;
+            return imaImagen;
         }
 
-        public void setX(int x) {
-            this.x = x;
+        public void setX(int iX) {
+            this.iX = iX;
         }
 
         public void setY(int y) {
-            this.y = y;
+            this.iY = y;
         }
         public int getY() {
-            return y;
+            return iY;
         }
 
         public int getX() {
-            return x;
+            return iX;
         }
 
         public void setDying(boolean dying) {
@@ -66,4 +92,65 @@ public class Sprite {
         public boolean isDying() {
             return this.dying;
         }
+        
+        /**
+     * getAncho
+     * 
+     * Metodo de acceso que regresa el ancho del icono 
+     * 
+     * @return un <code>entero</code> que es el ancho de la imagen.
+     * 
+     */
+    public int getAncho() {
+        ImageIcon imiIcon = new ImageIcon();
+        imiIcon.setImage(imaImagen);
+        return imiIcon.getIconWidth();
+        
+    }
+
+    /**
+     * getAlto
+     * 
+     * Metodo que  da el alto del icono 
+     * 
+     * @return un <code>entero</code> que es el alto de la imagen.
+     * 
+     */
+    public int getAlto() {
+        ImageIcon imiIcon = new ImageIcon();
+        imiIcon.setImage(imaImagen);
+        return imiIcon.getIconHeight();
+    }
+    
+        
+     /**
+     * intersecta
+     * 
+     * Método que sirve para saber si un objeto del mismo tipo colisiono
+     * con el objeto que invoca el método
+     * 
+     * @param objSprite
+     * @return True si hubo colision, false en otro caso
+     */
+    public boolean intersecta(Object objSprite) {
+        //valido si el objeto es de tipo personaje
+        if(objSprite instanceof Sprite) {
+            
+            //Creo los rectangulos de ambos objetos
+            Rectangle rctEste = new Rectangle(this.getX(), this.getY(), 
+                    this.getAncho(), this.getAlto());
+            Sprite sprMaloTemp = (Sprite) objSprite;
+            Rectangle rctOtro = new Rectangle(sprMaloTemp.getX(), 
+                    sprMaloTemp.getY(), sprMaloTemp.getAncho(), 
+                    sprMaloTemp.getAlto());
+            
+            //Regreso el valor de la funcion intersects, true si colisionaron,
+                    //false en otro caso
+            return rctEste.intersects(rctOtro);
+        }
+        
+        //Regreso false directamente si el objeto no era instancia de Personaje
+        return false;
+    }
+
 }
